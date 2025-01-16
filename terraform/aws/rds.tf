eca92bf73a7f485379fc29ba281db9e0fe4dcd47
Patrick Burke <24pburke@gmail.com>
2025-01-15 19:15:02 -0600

    Update rds.tf

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -4,7 +4,7 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
   backup_retention_period = 0
   storage_encrypted = false
   tags = {
-    git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
+    git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e52"
     git_file             = "terraform/aws/rds.tf"
     git_last_modified_at = "2021-12-08 23:26:32"
     git_last_modified_by = "tron47@gmail.com"
